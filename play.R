e07abaafc32649fe2e8bf97931ded307a1aebbd6
Harold Trammel <harold.trammel@aspca.org>
2015-09-25 18:27:42 -0500

    yet another step

--- play.R
+++ play.R
@@ -2,13 +2,15 @@
 
 fig <- 0
 tbl <- 0
-
 data("mtcars")
 require(car)
 require(pastecs)
 require(ggplot2)
 require(dplyr)
 require(knitr)
+
+# calculate standard error
+se <- function(x) sqrt(var(x)/length(x))
 
 desc_stat_lbl <- c("number of values (nbr.val) "
                     ,"number of null values (nbr.null) "
@@ -25,63 +27,86 @@ desc_stat_lbl <- c("number of values (nbr.val) "
                     ,"standard deviation (std.dev) "
                     ,"variation coefficient (coef.var)") 
 
-cars <- data.frame(mtcars)
-cars$cyl <- as.factor(cars$cyl)
-cars$vs <- as.factor(cars$vs)
-cars$am <- as.factor(cars$am)
-cars$gear <- as.factor(cars$gear)
-cars$carb <- as.factor(cars$carb)
+car_df <- data.frame(mtcars)
+car_df$cyl <- as.factor(car_df$cyl)
+car_df$vs <- as.factor(car_df$vs)
+car_df$am <- as.factor(car_df$am)
+car_df$gear <- as.factor(car_df$gear)
+car_df$carb <- as.factor(car_df$carb)
+
+levels(car_df$vs)[1] <- "vee"
+levels(car_df$vs)[2] <- "Straight"
+levels(car_df$am)[1] <- "Automatic"
+levels(car_df$am)[2] <- "Manual"
 
 ######## START STUFF ###########################
-stat_table <- data.frame(matrix(data=1:42, nrow=14, ncol=1))
-stat_table[,1] <- desc_stat_lbl
-stat_table <- cbind(stat_table, stat.desc(auto_df), stat.desc(manual_df))
-names(stat_table) <- c("Statistic", "Automatic", "Manual")
 
+am_stat <- car_df %>%
+    group_by(am) %>%
+    summarise_each(funs(mean, sd, se), mpg) %>%
+    rename( mpg_mn = mean, mpg_sd = sd, mpg_se = se )
 
-all_fit <- lm(mpg ~ . , data = cars)
+all_fit <- lm(mpg ~ . , data = car_df)
 all_fit_coef <- summary(all_fit)$coef
-print(kable(all_fit_coef, caption="All Fit Coefficients"))
+print(kable(all_fit_coef, 
+    caption="All Fit Coefficients")
+)
 
-nonfact_fit <- lm(mpg ~ wt + disp + drat , data = cars)
+all_cor <- cor(mtcars)
+mpg_cor <- matrix(all_cor$mpg, nrow=11, ncol=1, dimnames = list(rownames(all_cor),"mpg"))
+
+nonfact_fit <- lm(mpg ~ wt + disp + hp + drat , data = car_df)
 nonfact_fit_coef <- summary(nonfact_fit)$coef
-print(kable(nonfact_fit_coef, caption="Non-factor Fit Coefficients"))
+print(kable(nonfact_fit_coef, 
+    caption="Milage to Weight, Displacement, Horsepower, and Rear-Axle Ratio Coefficients")
+)
 
-fact_fit <- lm(mpg ~ am + gear + cyl + vs + carb , data = cars)
+fact_fit <- lm(mpg ~ am + gear + cyl + vs + carb , data = car_df)
 fact_fit_coef <- summary(fact_fit)$coef
-print(kable(fact_fit_coef, caption="Factor Fit Coefficients"))
+print(kable(fact_fit_coef, 
+    caption="Milage to Transmission Type, Gear Count, Cylinder Count, Carburator Count and Cylinder Arrangment Coefficients")
+)
 
-am_fit <- lm(mpg ~ am, data = cars)
+am_fit <- lm(mpg ~ am, data = car_df)
 am_fit_coef <- summary(am_fit)$coef
-print(kable(am_fit_coef, caption="Automatic vs Manual Coefficients"))
+print(kable(am_fit_coef, 
+    caption="Automatic vs Manual Coefficients")
+)
 
-auto_df <- cars %>% 
-            filter(am == 0) %>% 
-            select(one_of(c("mpg")))
-manual_df <- cars %>% 
-            filter(am == 1) %>% 
-            select(one_of(c("mpg")))
-
-
-# digits set to vector did not work
-# print(kable(stat_table, digits = c(0,0,0,2,2,2,2,2,4,4,4,4,4,4), row.names=F))
-print(kable(stat_table, digits = getOption("digits"), row.names=F))
-
-am_glm <- glm(mpg ~ am, data=cars, family="quasipoisson")
+am_glm <- glm(mpg ~ am, data=car_df, family="quasipoisson")
 #print(summary(glm(am_glm)))
 
+mpg_am_pl = ggplot(car_df, aes(x = mpg, fill = am)) + 
+    xlab("Miles per gallon") + 
+    ylab("Count of car_df") + 
+    geom_histogram(binwidth = 1) + 
+    #scale_x_continuous(breaks=0:1, labels=c("Automatic","Manual")) + 
+    facet_grid( am ~ . )
+    
+print(mpg_am_pl)
+
+# log_am = ggplot(car_df, aes(y = log(mpg))) + 
+#     ylab("Miles per gallon (log)") + 
+#     xlab("Automatic or Manual") + 
+#     geom_histogram() + 
+#     scale_x_continuous(breaks=0:1, labels=c("Automatic","Manual")) + 
+#     facet_grid( log(mpg) ~ am)
+# print(log_am)
 
 
-g_am = ggplot(cars, aes(x = factor(cyl), y = mpg)) + 
-    geom_smooth(method = "lm", aes(group = 1), colour = "black") +
-    geom_point(aes(y=mpg), size = 7, colour = "black", alpha=0.5) + 
-    geom_point(aes(x=factor(cyl)),size = 5, colour = "blue", alpha=0.6) +
-    ylab("Miles per gallon") + 
-    xlab("Number of Cylinders")  +
-    scale_manual(breaks=c(4,6,8), values=c("4 Cyl","6 Cyl", "8 Cyl"))
-print(g_am)
 
-# g_am = ggplot(cars, aes(x = am, y = mpg)) + 
+
+
+# g_am = ggplot(car_df, aes(x = factor(cyl), y = mpg)) + 
+#     geom_smooth(method = "lm", aes(group = 1), colour = "black") +
+#     geom_point(aes(y=mpg), size = 7, colour = "black", alpha=0.5) + 
+#     geom_point(aes(x=factor(cyl)),size = 5, colour = "blue", alpha=0.6) +
+#     ylab("Miles per gallon") + 
+#     xlab("Number of Cylinders")  +
+#     scale_manual(breaks=c(4,6,8), values=c("4 Cyl","6 Cyl", "8 Cyl"))
+# print(g_am)
+
+# g_am = ggplot(car_df, aes(x = am, y = mpg)) + 
 #     ylab("Miles per gallon") + 
 #     xlab("Automatic or Manual") + 
 #     geom_point(aes(y=mpg), size = 7, colour = "black", alpha=0.5) + 
@@ -90,7 +115,7 @@ print(g_am)
 #     geom_smooth(method = "lm", colour = "black") 
 # print(g_am)
 
-# log_am = ggplot(cars, aes(x = am, y = log(mpg))) + 
+# log_am = ggplot(car_df, aes(x = am, y = log(mpg))) + 
 #     ylab("Miles per gallon (log)") + 
 #     xlab("Automatic or Manual") + 
 #     geom_point(aes(y=log(mpg)), size = 7, colour = "black", alpha=0.5) + 
@@ -100,7 +125,7 @@ print(g_am)
 # print(log_am)
 
 # am_resid <- resid(am_fit)
-# amres_pl <- ggplot(cars, aes(x=am, y=am_resid)) +
+# amres_pl <- ggplot(car_df, aes(x=am, y=am_resid)) +
 #             ylab("Residuals") + 
 #             xlab("Automatic or Manual") +
 #             geom_hline(yintercept=0) +
